88961f05af7426145d7387e2af304641f5d1a247
Ebrahim Elsawy <elsawy@asait.co>
2019-09-24 00:13:58 +0200

    fix explore car status

--- application/models/Global_api_model.php
+++ application/models/Global_api_model.php
@@ -578,6 +578,7 @@ class Global_api_model extends CI_Model {
 					if($row->car_in_stock == 0){
 						$row->car_status = 2;
 					}
+					/*
 					$check_if_car_avalible = $this->checkIfCarAvalible($row->car_link);
 					if($check_if_car_avalible){
 						if($row->car_status == 1){
@@ -591,6 +592,24 @@ class Global_api_model extends CI_Model {
 							$row->cm_uid = $this->getCarModelNameByID($row->cm_uid);
 							$data['result'][] = $row;
 						}
+					}
+					*/
+					if($row->car_status == 0){
+						$check_if_car_avalible = $this->checkIfCarAvalible($row->car_link);
+						if($check_if_car_avalible){
+							$row->cb_uid = $this->getCarBrandNameByID($row->cb_uid);
+							$row->cm_uid = $this->getCarModelNameByID($row->cm_uid);
+							$row->car_status = 1;
+							$data['result'][] = $row;
+						}else{
+							$row->cb_uid = $this->getCarBrandNameByID($row->cb_uid);
+							$row->cm_uid = $this->getCarModelNameByID($row->cm_uid);
+							$data['result'][] = $row;
+						}
+					}else{
+						$row->cb_uid = $this->getCarBrandNameByID($row->cb_uid);
+						$row->cm_uid = $this->getCarModelNameByID($row->cm_uid);
+						$data['result'][] = $row;
 					}
 				}
 				$data['status'] = true;
@@ -1141,9 +1160,9 @@ class Global_api_model extends CI_Model {
 				$this->db->where('related_uid', $book_uid);
 				$this->db->update('invoices', $data);
 				
-				$data['status'] = true;
-				$data['message'] = "تم إلغاء الحجز بنجاح";
-				return $data;	
+				$result['status'] = true;
+				$result['message'] = "تم إلغاء الحجز بنجاح";
+				return $result;	
 			}else{
 				$data['result'] = [];
 				$data['status'] = false;
