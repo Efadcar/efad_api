f4c62493c9ad986e60397f712f8661b09a2cf66e
Ebrahim Elsawy <elsawy@asait.co>
2019-09-24 21:56:26 +0200

    add invoice_uid at booking

--- application/models/Global_api_model.php
+++ application/models/Global_api_model.php
@@ -1117,7 +1117,7 @@ class Global_api_model extends CI_Model {
 		if($q->num_rows() > 0) {
 			$row = $q->row();
 			$row->car_obj = json_decode($row->car_obj);
-			$this->db->select('invoice_total_fees,invoice_tax_total,invoice_total_fees_after_tax,invoice_payment_method');
+			$this->db->select('invoice_uid,invoice_total_fees,invoice_tax_total,invoice_total_fees_after_tax,invoice_payment_method');
 			$m = $this->db->get_where('invoices',array("related_uid" => $row->book_uid,"member_uid" => $member_uid));
 			if($m->num_rows() > 0) {
 				$mrow = $m->row();
